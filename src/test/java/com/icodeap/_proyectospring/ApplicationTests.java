d13a3437c0b887148e276bb2cb9f50a614034b1f
yaselramos <yaselramos@gmail.com>
2025-09-09 18:13:04 -0300

    primer commir

---
+++ src/test/java/com/icodeap/_proyectospring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap._proyectospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
